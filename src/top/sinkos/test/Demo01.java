6f330c175cc123cd9ab113d98ea94274d9d9d2e2
sinkos <1375105914@qq.com>
2022-07-11 21:42:30 +0800

    第二次在idea上向git的本地仓库提交 Demo01.java文件

--- src/top/sinkos/test/Demo01.java
+++ src/top/sinkos/test/Demo01.java
@@ -3,5 +3,6 @@ package top.sinkos.test;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("git_test-----01");
+        System.out.println("git_test-----02");
     }
 }
